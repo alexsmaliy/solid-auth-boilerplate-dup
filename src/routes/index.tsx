3fe50a36ae6cfe13192ee833fb7bec9bb1a09674
Alex Smaliy <alex.smaliy@gmail.com>
2023-01-21 23:39:05 -0800

    test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,24 +2,27 @@ import { Database } from "@cloudflare/d1";
 import { FormError, useRouteData } from "solid-start";
 import { createServerAction$, createServerData$, redirect, ServerFunctionEvent } from "solid-start/server";
 import { COOKIE_NAME, parseCookie, serializeCookie } from "~/auth/cookies";
-import { deleteSession, getUserBySessionId } from "~/database/operations";
+import { deleteSession, getUserBySessionId, User } from "~/database/operations";
 
 async function redirectIfSessionIsInvalid(_unused: any, event: ServerFunctionEvent) {
-  const { env, request } = event;
-  const d1_binding_from_env = (env as any).TESTDB;
-  const d1 = new Database(d1_binding_from_env);
+  // const { env, request } = event;
+  // const d1_binding_from_env = (env as any).TESTDB;
+  // const d1 = new Database(d1_binding_from_env);
 
-  const cookieString = request.headers.get("Cookie") || "";
-  const parsedCookies = parseCookie(cookieString);
-  const sessionCookie = parsedCookies[COOKIE_NAME] || "";
+  // const cookieString = request.headers.get("Cookie") || "";
+  // const parsedCookies = parseCookie(cookieString);
+  // const sessionCookie = parsedCookies[COOKIE_NAME] || "";
 
-  const dbResponse = await getUserBySessionId(d1, sessionCookie);
-  if (dbResponse instanceof Error) throw new FormError(dbResponse.message); // not the right error to throw?
-  const user = dbResponse;
+  // const dbResponse = await getUserBySessionId(d1, sessionCookie);
+  // if (dbResponse instanceof Error) throw new FormError(dbResponse.message); // not the right error to throw?
+  // const user = dbResponse;
 
-  const valid = user !== null;
+  // const valid = user !== null;
+  const valid = true;
   if (!valid) throw redirect("/login");
-  return user;
+  // return user;
+  const dummy: User = {userName: "user", userId: 123, passwordHash: "hash123"};
+  return dummy;
 }
 
 export function routeData() {
