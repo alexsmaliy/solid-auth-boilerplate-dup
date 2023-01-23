e738149f199a1be2bb642b6a987445e27208a9eb
Alex Smaliy <alex.smaliy@gmail.com>
2023-01-23 15:16:30 -0800

    with types

--- src/d1/operations.tsx
+++ src/d1/operations.tsx
@@ -1,3 +1,5 @@
+import { D1Database } from "@cloudflare/workers-types";
+
 const SQL_TEMPLATE_STRINGS = {
     CHECK_USER:
         `SELECT
@@ -15,14 +17,30 @@ const SQL_TEMPLATE_STRINGS = {
             user_name = ?;`,
 };
 
-export async function getUserByUsername(db: any, username: string) {
+type UserDbRow = {
+    user_id: number,
+    user_name: string,
+    password_hash: string,
+}
+
+export type User = {
+    userId: number,
+    userName: string,
+    passwordHash: string,
+};
+
+function toUser({user_id, user_name, password_hash}: UserDbRow): User {
+    return { userId: user_id, userName: user_name, passwordHash: password_hash };
+}
+
+export async function getUserByUsername(db: D1Database, username: string) {
     const stmt = db.prepare(SQL_TEMPLATE_STRINGS.GET_USER_BY_USERNAME).bind(username);
-    return stmt.all().then(res => {
+    return stmt.all<UserDbRow>().then(res => {
         if (res.results !== undefined && res.results.length === 0)
-            return Error("No such user!\n" + JSON.stringify({res, stmt}));
+            return Error("No such user!");
         else if (res.results !== undefined)
-            return res.results[0];
+            return toUser(res.results[0]);
         else
-            return JSON.stringify({res, stmt});
+            return Error(`Some other problem: ${JSON.stringify(res)}`);
     }).catch(err => Error(err.message));
 }
